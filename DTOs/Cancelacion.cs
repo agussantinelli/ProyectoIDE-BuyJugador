17be9cae9e0e4850f22f950b85f7460a51790ff5
Ratti Martin <66048377+MartinUTN@users.noreply.github.com>
2025-07-22 08:49:21 -0300

    Delete DTOs/Cancelacion.cs

--- DTOs/Cancelacion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DTO
-{
-    public class Cancelacion
-    {
-        public int NroVenta { get; private set; }
-        public DateTime Fecha { get; private set; }
-        public string Motivo { get; private set; }
-    }
-}
