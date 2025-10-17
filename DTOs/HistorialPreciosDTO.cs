0ddb99b3b101bbee3370da81af0cacc92eb789fb
Ratti Martin <maratti@frro.utn.edu.ar>
2025-10-17 12:11:07 -0300

    Proveedor Inactivo OK

--- DTOs/HistorialPreciosDTO.cs
+++ DTOs/HistorialPreciosDTO.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 
 namespace DTOs
 {
-    // # Representa un único punto en el gráfico (Fecha, Monto).
     public class PrecioPuntoDTO
     {
         public DateTime Fecha { get; set; }
         public decimal Monto { get; set; }
     }
 
-    // # Contiene la información de un producto y su lista de puntos de precios.
     public class HistorialPrecioProductoDTO
     {
         public int IdProducto { get; set; }
